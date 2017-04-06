83d5cdc98df8f3601898206bfbcac06962d3dd12
Ming Liu <vmliu1@gmail.com>
2017-04-05 23:03:00 -0700

    Scroll to anchor

--- addon/routes.js
+++ addon/routes.js
@@ -4,10 +4,18 @@ export default buildRoutes(function() {
   this.route('projects', { path: '/projects/:projectName' }, function() {
     this.route('modules', { path: '/modules/:moduleName' }, function() {
       this.route('classes', { path: '/classes/:className' }, function() {
-        this.route('methods');
-        this.route('properties');
-        this.route('attributes');
-        this.route('events');
+        this.route('methods', function() {
+          this.route('anchor', { path: '/:anchorName' });
+        });
+        this.route('properties', function() {
+          this.route('anchor', { path: '/:anchorName' });
+        });
+        this.route('attributes', function() {
+          this.route('anchor', { path: '/:anchorName' });
+        });
+        this.route('events', function() {
+          this.route('anchor', { path: '/:anchorName' });
+        });
       });
     });
   });
--- addon/routes/projects/modules/classes.js
+++ addon/routes/projects/modules/classes.js
@@ -5,5 +5,37 @@ export default Ember.Route.extend({
 
   model({ className }) {
     return this.get('store').peekRecord('class', className);
+  },
+
+  setupController(controller, model, transition) {
+    this._super(...arguments);
+
+    this.updateAnchor(transition);
+  },
+
+  updateAnchor(transition) {
+    let { targetName, params } = transition;
+
+    let anchor = targetName.match(/\.anchor$/) ? (params[targetName].anchorName || null) : null;
+
+    this.set('controller.anchor', anchor);
+  },
+
+  actions: {
+    willTransition(transition) {
+      this.updateAnchor(transition);
+    },
+
+    didTransition() {
+      let anchor = this.get('controller.anchor');
+
+      if (anchor) {
+        Ember.run.schedule('afterRender', function() {
+          let selector = `#ember-doc-engine [data-anchor="${anchor}"]`;
+
+          Ember.$(selector).get(0).scrollIntoView();
+        });
+      }
+    }
   }
 });
